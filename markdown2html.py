6991aa9a2f346f4e061e2303ff52c07eb49f83ba
RuslanHamidov <rhamidov202@gmail.com>
2024-02-13 17:50:40 +0400

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -11,4 +11,4 @@ import sys
 if __name__ == "__main__":
   args = sys.argv
   if len(args) != 2:
-      sys.stdout.write("Usage: ./markdown2html.py README.md README.html")
+      sys.stdout.write("Usage: ./markdown2html.py README.md README.html ")
