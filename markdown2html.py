d6aff245e18edcf5b5e5d998070421871305c4d7
RuslanHamidov <rhamidov202@gmail.com>
2024-02-13 18:07:37 +0400

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -5,10 +5,18 @@
   python3 -c 'print(__import__("my_module").__doc__)'
 
 """
-
+import os
 import sys
 
 if __name__ == "__main__":
   args = sys.argv
-  if len(args) != 2:
-      sys.stdout.write("Usage: ./markdown2html.py README.md README.html\n")
+  if len(args) < 2:
+      sys.stderr("Usage: ./markdown2html.py README.md README.html\n")
+      exit(1)
+
+  if not os.path.exists(args[1]):
+     sys.stderr("Missing" + args[1] +"\n")
+     exit(1)
+
+  exit(0)
+
