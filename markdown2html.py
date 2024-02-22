d10d8f50b0df1194a4d44bdbae35306839d98137
RuslanHamidov <rhamidov202@gmail.com>
2024-02-22 14:38:03 +0400

    add hash to html

--- markdown2html.py
+++ markdown2html.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
-
-
 ''' python3 -c 'print(__import__("my_module").__doc__)' '''
-
 
 import os
 import sys
@@ -11,7 +8,6 @@ import re
 
 with open(sys.argv[1], "r") as readme_file:
   text = readme_file.read()
-  
   
   def toHtml(textfile):
     hashes = re.findall(r'(#+)\s*(.*)', textfile)
@@ -21,12 +17,8 @@ with open(sys.argv[1], "r") as readme_file:
         html_content += f"<h{hash_level}>{title.strip()}</h{hash_level}>\n"
     return html_content
   
-  
   html = toHtml(text)
   
-   
-
-
 if __name__ == "__main__":
   args = sys.argv
   if len(args) < 3:
@@ -37,7 +29,6 @@ if __name__ == "__main__":
      sys.stderr.write("Missing " + args[1] +"\n")
      exit(1)
      
-  
   with open(sys.argv[2], 'w') as html_file:
     html_file.write(html)
   
