303346b4fd2060e53a3bc52b4eeeb985662d4ec3
RuslanHamidov <rhamidov202@gmail.com>
2024-02-22 14:26:30 +0400

    add hash to html

--- markdown2html.py
+++ markdown2html.py
@@ -10,21 +10,11 @@ import sys
 import re
 
 
+readme = sys.argv[1]
+htmlfile = sys.argv[2]
 
-
-
-if __name__ == "__main__":
-  args = sys.argv
-  if len(args) < 3:
-      sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-      exit(1)
-
-  if not os.path.exists(args[1]):
-     sys.stderr.write("Missing " + args[1] +"\n")
-     exit(1)
-     
-  with open("fn_md", "r") as f:
-    text = f.read()
+with open(readme, "r") as readme_file:
+  text = readme_file.read()
   
   ## Parse from README to HTML
   def toHtml(textfile):
@@ -38,9 +28,21 @@ if __name__ == "__main__":
   #Function to parse
   html = toHtml(text)
    
+## Starting point of script
+
+if __name__ == "__main__":
+  args = sys.argv
+  if len(args) < 3:
+      sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+      exit(1)
+
+  if not os.path.exists(args[1]):
+     sys.stderr.write("Missing " + args[1] +"\n")
+     exit(1)
+     
   
-  with open('fn_html', 'w') as f:
-    f.write(html)
+  with open(htmlfile, 'w') as html_file:
+    html_file.write(html)
   
   exit(0)
 
