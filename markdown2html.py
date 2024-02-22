cc104062122b59c5aab3866d769521b25b08e171
RuslanHamidov <rhamidov202@gmail.com>
2024-02-22 21:26:56 +0400

    add star to html

--- markdown2html.py
+++ markdown2html.py
@@ -23,6 +23,13 @@ def parse_section(section_text):
         for _, words in lines_with_dashes:
             html_content += f"    <li>{words.strip()}</li>\n"
         html_content += "</ul>\n"
+        
+    lines_with_stars = re.findall(r'(*+)\s*(.*)', section_text)
+    if lines_with_stars:
+        html_content += "<ol>\n"
+        for _, words in lines_with_stars:
+            html_content += f"    <li>{words.strip()}</li>\n"
+        html_content += "</ol>\n"
 
     return html_content
 
