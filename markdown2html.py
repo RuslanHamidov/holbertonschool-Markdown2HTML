18cf9f7ed6f6bd0939a15e2c2c2a8b65c596c453
RuslanHamidov <rhamidov202@gmail.com>
2024-02-22 14:45:24 +0400

    add hash to html

--- markdown2html.py
+++ markdown2html.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 
-'''python3 -c 'print(__import__("my_module").__doc__)' '''
+'''Script that takes an argument 2 strings:
+First argument is the name of the Markdown file
+Second argument is the output file name
+script converts the markdown file to HTML
+The script will convert the following markdown syntax:
+- Headers: Any number of #s at the start of a line
+- Unordered listing: Lines that start with a dash (-)
+- Ordered listing: Lines that start with a star (*)
+- Text in double asterisks (**) is bold
+- Text in double underscores (__) is italic
+- Text in double brackets (()) is hashed
+- Text in double parentheses (()) is case insensitive
+The script will convert the markdown file to HTML and write it to the output file
+If the markdown file does not exist, the script will print Missing <filename> and exit with a status code of 1
+If the output file already exists, it will be overwritten
+The script will exit with a status code of 0 upon success
+
+Usage: ./markdown2html.py README.md README.html 
+'''
 
 import os
 import sys
